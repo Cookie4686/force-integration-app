575ca3cd0656328640ac15bcb0250e61e93e0303
Cookie4686 <cookieandcake1105@gmail.com>
2026-09-16 20:08:58 +0700

    add focus point radius and model type selector Add focus point radius to pose detection Add model type selector to config section (lite, full, heavy) Cleanup some state code in use-mediapipe-pose hooks Fix breadcrumb header to be sticky Fix tool-pose page sections to scroll independently of each other

--- src/app/tool/pose/image-section.tsx
+++ src/app/tool/pose/image-section.tsx
@@ -3,6 +3,7 @@
 import { cn } from "cn";
 import { CrosshairIcon } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import useMediapipePose from "@/hooks/use-mediapipe-pose";
@@ -18,14 +19,16 @@ export default function PageToolPoseSectionImage({
 }) {
 	const { refs, states, handlers } = mediapipePose;
 	const { imageRef, canvasRef } = refs;
-	const { modelStatus, focusPoint } = states;
+	const { modelStatus, focusPoint, focusPointRadius } = states;
 	const { handleCanvasClick, handleClearFocusPoint, handleOnImageLoad } = handlers;
+	const [isShowSkeletal, setIsShowSkeletal] = useState(true);
+	const [isShowFocusPoint, setIsShowFocusPoint] = useState(true);
 
 	return (
-		<div className="relative flex w-full flex-col items-center justify-center p-2">
+		<div className="relative flex h-full w-full flex-col items-center justify-center p-2">
 			<div className="relative inline-block max-w-full">
 				<Image
-					className="block max-h-128 w-auto max-w-full rounded object-contain"
+					className="block max-h-196 w-auto max-w-full rounded object-contain"
 					loading="eager"
 					width={512}
 					height={512}
@@ -37,23 +40,49 @@ export default function PageToolPoseSectionImage({
 				<canvas
 					className={cn(
 						"absolute top-0 left-0 h-full w-full rounded",
-						modelStatus.state === "inference" && "invisible"
+						(modelStatus.state !== "idle" || !isShowSkeletal) && "opacity-0"
 					)}
 					ref={canvasRef}
 					onClick={handleCanvasClick}
 				/>
-				{focusPoint && (
-					<CrosshairIcon
-						className="absolute -translate-1/2 stroke-white"
-						size={24}
-						style={{ left: focusPoint.x, top: focusPoint.y }}
-					/>
+				{focusPoint && isShowFocusPoint && (
+					<div
+						className="pointer-events-none absolute flex -translate-1/2 items-center justify-center rounded-full border border-white"
+						style={{
+							width: focusPointRadius * 2,
+							height: focusPointRadius * 2,
+							left: focusPoint.x,
+							top: focusPoint.y,
+						}}
+					>
+						<CrosshairIcon className="stroke-white" size={24} />
+					</div>
 				)}
-				{focusPoint && (
-					<Button className="absolute right-4 bottom-4" onClick={handleClearFocusPoint}>
-						Clear Focus Point
+				<div className="pointer-events-none absolute right-4 bottom-4 flex flex-col gap-2">
+					{focusPoint && (
+						<Button className="pointer-events-auto" onClick={handleClearFocusPoint}>
+							Clear Focus Point
+						</Button>
+					)}
+					{focusPoint && (
+						<Button
+							className="pointer-events-auto"
+							onClick={() => {
+								setIsShowFocusPoint((prev) => !prev);
+							}}
+						>
+							{isShowFocusPoint ? "Hide" : "Show"} Focus Point
+						</Button>
+					)}
+					<Button
+						className="pointer-events-auto"
+						onClick={() => {
+							setIsShowSkeletal((prev) => !prev);
+						}}
+					>
+						{isShowSkeletal ? "Hide" : "Show"} Skeletal
 					</Button>
-				)}
+				</div>
 			</div>
 		</div>
 	);
